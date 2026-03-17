8680cca322a854e84bcbfda8bfbf0acd335dd681
Lerivd <lrivadeneira.pe@hotmail.com>
2026-03-17 07:25:15 -0500

    feat: se agregaron los archivos necesarios para poder empezar

---
+++ src/test/java/com/leonardo/core/DriverManager.java
@@ -0,0 +1,73 @@
+package com.leonardo.core;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+    private static WebDriver driver;
+    private static Scenario scenario;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    @Before(order = 0)
+    public void setUp() {
+
+        System.out.println("Codigo ejecutado automaticamente, antes de ejecutar los features");
+
+        // Detectar SO
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            // Windows OS
+            System.setProperty("webdriver.chrome.driver", "PATH");
+        } else if (os.contains("mac")) {
+            // Mac OS
+            System.setProperty("webdriver.chrome.driver", "PATH");
+        } else {
+            throw new IllegalStateException("Unsupported operating system: " + os);
+        }
+
+        // HTTP Factory
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Before(order = 1)
+    public void setScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
+
+    @After
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    public static void screenShot() {
+        byte[] evidencia = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(evidencia, "image/png", "evidencias");
+    }
+
+    public static void scrollDown() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // This  will scroll down the page by  1000 pixel vertical
+        js.executeScript("window.scrollBy(0,1000)");
+    }
+
+    public static void esperaImplicita() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
